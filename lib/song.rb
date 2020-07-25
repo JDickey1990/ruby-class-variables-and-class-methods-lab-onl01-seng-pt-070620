2718cb56b917e7ae665fc3bf1839f21a17347e9d
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-25 15:03:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  
+ @@count= 0
+ @@artists=[]
+ @@genres=[]
+ 
+  def initialize(name, artist, genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@count +=1
+    @@artists<< artist
+    @@genres<< genre
+  end
+
+attr_accessor :name, :artist, :genre
+
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+    
+  
+  
+
+
+end
